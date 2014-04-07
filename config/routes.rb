8f50526f85fa7516100ed52bc538d60e7bd8abdc
Josh Rendek <josh@bluescripts.net>
2014-04-07 01:13:48 -0400

    work on ui and theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ContactManager::Application.routes.draw do
   resources :contacts
 
+  root 'contacts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
